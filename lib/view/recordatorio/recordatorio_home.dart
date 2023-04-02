abb69e4c2640a7fc6a71305d7406cc1b424c20d9
VenenoParaLasHadas <rodrigo.olarteastudillo@protonmail.com>
2023-04-02 13:55:27 -0600

    agrego la ventana de recordatorios con variables de ejemplo

--- lib/view/recordatorio/recordatorio_home.dart
+++ lib/view/recordatorio/recordatorio_home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dosis_exacta/viewmodel/home_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -20,12 +22,38 @@ class _RecordatorioHome extends State<RecordatorioHome> {
   HomeVM viewModel = HomeVM();
   var user;
 
+
+  var time = ["18:15","1/3","19:23"];
+  var medicine = ["Paracetamol","Luvox","Buscapina"];
+  var time_left = ["Dentro de 2 horas","en 10 minutos","Dentro de 3 minutos"];
+  var indicacions = ["2 tabletas","Media tableta","1 tableta"];
+
+  var mostrado = [0,0,0];
+
   onClickEdit() {
     Navigator.of(context).pushNamed("/recordatorio/list");
   }
 
   onClickReturn() {
     Navigator.of(context).pop();
+  }
+
+  onClickingested(int index){
+    mostrado[index] = 1;
+  }
+
+  refreshRemainders() async{
+    setState(() {
+      for(var i = 0; i<medicine.length;i++){
+        if(mostrado[i]==1){
+          medicine.removeAt(i);
+          time.removeAt(i);
+          time_left.removeAt(i);
+          indicacions.removeAt(i);
+          mostrado.removeAt(i);
+        }
+      }
+    });
   }
 
   @override
@@ -71,7 +99,81 @@ class _RecordatorioHome extends State<RecordatorioHome> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 0.75.sh),
+              SizedBox(height: 0.01.sh),
+              SizedBox(
+                height: 0.70.sh,
+                child: ListView.builder(
+                  itemCount: medicine.length,
+                  itemBuilder: (context,index){
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(0.03.sw, 0, 0.03.sw, 0),
+                    child: Card(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0.1.sw, 0.02.sh, 0.1.sw, 0.0.sh),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(medicine[index],style:AppTextTheme.large(),maxLines: 3,
+                                    overflow: TextOverflow.clip,
+                                  ),
+                                ),
+                                Container(
+                                  width: 50,
+                                  child: Text(time[index],style:AppTextTheme.small()),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0.1.sw, 0.005.sh, 0.1.sw, 0.0.sh),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  child: Text(time_left[index],style:AppTextTheme.small(),maxLines: 3),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0.1.sw, 0.005.sh, 0.1.sw, 0.0.sh),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(indicacions[index],style:AppTextTheme.small(),maxLines: 3,overflow: TextOverflow.clip,),
+                                )
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0.1.sw, 0.005.sh, 0.1.sw, 0.0.sh),
+                            child: SizedBox(
+                              width: 0.55.sw,
+                              child: ElevatedButton(
+                                onPressed: (){
+                                  onClickingested(index);
+                                  refreshRemainders();
+                                },
+                                style: Styles.button(context, color: Color(0xFF44BBA4)),
+                                child: Text("Dosis ingerida",style:AppTextTheme.medium(color: Colors.white)),
+
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                  },
+                ),
+              ),
+
+              SizedBox(height: 0.03.sh),
               Padding(
                 padding: EdgeInsets.fromLTRB(0.1.sw, 0, 0.1.sw, 0),
                 child: ElevatedButton(
