630058a9c2ff718e41afd85c5adc64108de1b49d
VenenoParaLasHadas <rodrigo.olarteastudillo@protonmail.com>
2023-04-02 18:59:58 -0600

    Se agregó la vista de modificar medicamentos

--- lib/view/recordatorio/recordatorio_list.dart
+++ lib/view/recordatorio/recordatorio_list.dart
@@ -18,6 +18,10 @@ class _RecordatorioList extends State<RecordatorioList> {
   bool isLoading = true;
   HomeVM viewModel = HomeVM();
   var user;
+  var time = ["8:00","10:15",""].cast<dynamic>();
+  var medicine = ["Paracetamol","Luvox","Buscapina"].cast<dynamic>();
+  var time_left = ["Cada 8 horas","Cada 12 horas","3 veces al dia"].cast<dynamic>();
+  var indicacions = ["2 tabletas","Media tableta","1 tableta"].cast<dynamic>();
   onClickAddHand (){
     Navigator.of(context).pushNamed("/recordatorio/form");
   }
@@ -32,6 +36,15 @@ class _RecordatorioList extends State<RecordatorioList> {
 
   onClickReturn() {
     Navigator.of(context).pop();
+  }
+
+  onClickDelete(int index) async{
+    setState(() {
+      medicine.removeAt(index);
+      time.removeAt(index);
+      time_left.removeAt(index);
+      indicacions.removeAt(index);
+    });
   }
 
   @override
@@ -77,7 +90,97 @@ class _RecordatorioList extends State<RecordatorioList> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 0.75.sh),
+              SizedBox(height: 0.01.sh),
+              SizedBox(
+                height: 0.70.sh,
+                child: ListView.builder(
+                  itemCount: medicine.length,
+                  itemBuilder: (context,index){
+                    return Padding(
+                      padding: EdgeInsets.fromLTRB(0.03.sw, 0, 0.03.sw, 0),
+                      child: Card(
+                        elevation: 5,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0.1.sw, 0.02.sh, 0.1.sw, 0.0.sh),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(medicine[index],style:AppTextTheme.large(),maxLines: 3,
+                                      overflow: TextOverflow.clip,
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 50,
+                                    child: Text(time[index],style:AppTextTheme.small()),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0.1.sw, 0.005.sh, 0.1.sw, 0.0.sh),
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    child: Text(time_left[index],style:AppTextTheme.small(),maxLines: 3),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0.1.sw, 0.005.sh, 0.1.sw, 0.0.sh),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(indicacions[index],style:AppTextTheme.small(),maxLines: 3,overflow: TextOverflow.clip,),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0.1.sw, 0.005.sh, 0.1.sw, 0.0.sh),
+                              child: Row(
+                                children: [
+                                  Padding(
+                                      padding:EdgeInsets.fromLTRB(0.003.sw, 0.005.sh, 0.0.sw, 0.0.sh),
+                                      child: Row(
+                                        children: [
+                                          SizedBox(
+                                            width: 0.3.sw,
+                                            child: ElevatedButton(
+                                                onPressed: onClickAddHand,
+                                                style: Styles.button(context, color: Color(0xFF7977AA)),
+                                                child: Text("Editar",style:AppTextTheme.medium(color: Colors.white))
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 0.08.sw,
+                                          ),
+                                          SizedBox(
+                                            width: 0.3.sw,
+                                            child: ElevatedButton(
+                                                onPressed: (){onClickDelete(index);},
+                                                style: Styles.button(context, color: Color(0xFFF44336)),
+                                                child: Text("Eliminar",style:AppTextTheme.medium(color: Colors.white))
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
               Padding(
                 padding: EdgeInsets.fromLTRB(0.1.sw, 0, 0.1.sw, 0),
                 child: ElevatedButton(
