a552d5713a882ab24fdad96262fe92e1675c4641
AlexDsJz <cduenasj1700@alumno.ipn.mx>
2023-04-13 17:02:02 -0600

    Functionality was added to take a photo and store it in a variable.

--- lib/view/recordatorio/recordatorio_photo.dart
+++ lib/view/recordatorio/recordatorio_photo.dart
@@ -4,10 +4,9 @@ import 'package:dosis_exacta/view/recordatorio/recordatorio_photo_resultados.dar
 import 'package:dosis_exacta/viewmodel/home_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:camera/camera.dart';
 
-import '../common/theme.dart'; // Los temas personalizados
-
-import 'package:camera/camera.dart'; // Para el manejo de la camara
+import '../common/theme.dart';
 
 class RemainderPhoto extends StatefulWidget {
   const RemainderPhoto({Key? key}) : super(key: key);
@@ -21,28 +20,40 @@ class StateRemainderPhoto extends State<RemainderPhoto> {
   HomeVM viewModel = HomeVM();
   var user;
 
-  late CameraController controller; // Para controlar la camara
-  late Future<void> initializeControllerFuture; // Para inicializar la camara
+  late CameraController controller;
+  late Future<void> initializeControllerFuture;
 
-  // Regresa a la pantalla anterior
+  late XFile? imageFile; // Variable para almacenar la imagen
+
+  StateRemainderPhoto() {
+    initializeCamera();
+  }
+
   returnMenu() {
     Navigator.of(context).pop();
+  }
+
+  Future<void> initializeCamera() async {
+    final cameras = await availableCameras();
+    final firstCamera = cameras.first;
+    controller = CameraController(firstCamera, ResolutionPreset.medium);
+    initializeControllerFuture = controller.initialize();
   }
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance?.addPostFrameCallback(
-      (_) async {
+            (_) async {
           user = await viewModel.checkExistingUser();
           if(user == null) {
             Navigator.of(context).pushReplacementNamed("/");
           }
           else {
             setState(
-              () {
-                isLoading = false;
-              }
+                    () {
+                  isLoading = false;
+                }
             );
             initializeCamera();
           }
@@ -50,67 +61,60 @@ class StateRemainderPhoto extends State<RemainderPhoto> {
     );
   }
 
-  Future<void> initializeCamera() async {
-    final cameras = await availableCameras(); // Obtiene una lista de las cámaras disponibles en el dispositivo.
-    final firstCamera = cameras.first;   // Obtiene una cámara específica de la lista de cámaras disponibles
-    controller = CameraController(firstCamera, ResolutionPreset.medium); // Crea el controlador de la camara
-    initializeControllerFuture = controller.initialize(); // Inicializa el controlador
-  }
-
   @override
   void dispose() {
-    controller.dispose(); // Destruccion del controlador
+    controller.dispose();
     super.dispose();
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return isLoading ? const Scaffold() : Scaffold(
-        appBar: AppBar( // La barra de titulo
-          automaticallyImplyLeading: false,
-          backgroundColor: AppColors.primary(), // Color de fondo
-          title: Row( // Seccion del titulo se llena en fila
-            mainAxisAlignment: MainAxisAlignment.center, // Centrado horizontalmente
-            children: <Widget> [ // Contiene
-              Text( // Texto
-                  "Fotografia", // Cadena de contenido
-                  style: AppTextTheme.medium(color: Colors.white) // Formato color blanco
-              )
-            ],
-          ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: AppColors.primary(),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Text(
+                "Fotografia",
+                style: AppTextTheme.medium(color: Colors.white)
+            )
+          ],
         ),
-        body: FutureBuilder<void>( // Constructor de un evento futuro
-          future: initializeControllerFuture, // El evento futuro es la inicializacion de la camara
-          builder: (context, snapshot) { // Constructor anonimo
-            if (snapshot.connectionState == ConnectionState.done) { // Si el Future está completo
-              return CameraPreview(controller); // muestra la vista previa
-            } else { // Si aun no se inicializa
-              return const Center(child: CircularProgressIndicator()); // Muestra un indicador de carga
-            }
-          },
-        ),
-      floatingActionButton: FloatingActionButton( // Boton camara
-        onPressed: () async { // Funcion anonima para tomar la fotografia
-          try { // Intenta
-            await initializeControllerFuture; // Esperar a que se inicialice la camara
-            final image = await controller.takePicture(); // Tomar la fotografia
-            if (!mounted) return; // Al ser asincrono si ya no esta activo el widget termina
-            await Navigator.of(context).push( // Muestra la siguiente pantalla
-              MaterialPageRoute( // Crea la pantalla
-                builder: (context) => RemainderPhotoResults( // Construye el widget
-                  imagePath: image.path, // Le envia la ruta de la imagen
+      ),
+      body: FutureBuilder<void>(
+        future: initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await initializeControllerFuture;
+            final image = await controller.takePicture(); // Toma la fotografia
+            if (!mounted) return;
+            setState(() {
+              imageFile = XFile(image.path); // Asigna la imagen a la variable
+            });
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => RemainderPhotoResults(
+                  imagePath: image.path,
                 ),
               ),
             );
-          } catch (e) { // Si ocurre un error
-            print(e); // Lo muestra en la consola
+          } catch (e) {
+            print(e);
           }
         },
         child: const Icon(Icons.camera_alt),
       ),
     );
   }
-
 }
